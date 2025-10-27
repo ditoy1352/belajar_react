d81cd1ed62fc70dc85077335a0192f2122bd0b5f
ditoy <dito030405@gmail.com>
2025-10-27 22:41:49 +0700

    belajar react

--- src/App.js
+++ src/App.js
@@ -6,42 +6,43 @@ const Button = ({ children }) => {
   return <button className="button">{children}</button>;
 };
 
-const Square = ({ number }) => {
-  const [value, setValue] = useState(null);
-
-  const handleClick = () => {
-    setValue('X');
-  };
-
+const Square = ({ number, onClick }) => {
   return (
-    <div className="square" onClick={handleClick}>
-      {value}
+    <div className="square" onClick={onClick}>
+      {number}
     </div>
   );
 };
 
 const App = () => {
+
+  const [isXNext, setIsXNext] = useState(false);
+  const handlesquareClick = () => {
+    setIsXNext(!isXNext);
+    console.log("handle click");
+  };
+
   return (
     <div className="container">
       <div className="board">
         <div className="row">
-          <Square number={1} />
-          <Square number={2} />
-          <Square number={3} />
+          <Square number={1} onClick={handlesquareClick}/>
+          <Square number={2} onClick={handlesquareClick}/>
+          <Square number={3} onClick={handlesquareClick}/>
         </div>
         <div className="row">
-          <Square number={4} />
-          <Square number={5} />
-          <Square number={6} />
+          <Square number={4} onClick={handlesquareClick}/>
+          <Square number={5} onClick={handlesquareClick}/>
+          <Square number={6} onClick={handlesquareClick}/>
         </div>
         <div className="row">
-          <Square number={7} />
-          <Square number={8} />
-          <Square number={9} />
+          <Square number={7} onClick={handlesquareClick}/>
+          <Square number={8} onClick={handlesquareClick}/>
+          <Square number={9} onClick={handlesquareClick}/>
         </div>
       </div>
       <div className="info">
-        <div className="status">Next player: X</div>
+        <div className="status">Next player: {isXNext ? "X" : "O"}</div>
         <Button className="">New Game</Button>
 
         <div className="history">
