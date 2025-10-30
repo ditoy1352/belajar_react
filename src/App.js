61a98f43bde67141266f02dd9d199458d57f802d
ditoy <dito030405@gmail.com>
2025-10-30 22:30:49 +0700

    Refactor App component structure and improve button functionality

--- src/App.js
+++ src/App.js
@@ -17,34 +17,45 @@ const Square = ({ number, onClick }) => {
 const App = () => {
 
   const [isXNext, setIsXNext] = useState(false);
-  const handlesquareClick = () => {
-    setIsXNext(!isXNext);
-    console.log("handle click");
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const handlesquareClick = (index) => {
+    setBoard((currboard) => {
+      const boardCopy = [...currboard];
+
+      if (boardCopy[index]) {
+        return boardCopy;
+      };
+      
+      boardCopy[index] = isXNext ? "X" : "O";
+
+      setIsXNext(!isXNext);
+
+      return boardCopy;
+    })
   };
 
   return (
     <div className="container">
       <div className="board">
         <div className="row">
-          <Square number={1} onClick={handlesquareClick}/>
-          <Square number={2} onClick={handlesquareClick}/>
-          <Square number={3} onClick={handlesquareClick}/>
+          <Square number={board[0]} onClick={() => handlesquareClick(0)}/>
+          <Square number={board[1]} onClick={() => handlesquareClick(1)}/>
+          <Square number={board[2]} onClick={() => handlesquareClick(2)}/>
         </div>
         <div className="row">
-          <Square number={4} onClick={handlesquareClick}/>
-          <Square number={5} onClick={handlesquareClick}/>
-          <Square number={6} onClick={handlesquareClick}/>
+          <Square number={board[3]} onClick={() => handlesquareClick(3)}/>
+          <Square number={board[4]} onClick={() => handlesquareClick(4)}/>
+          <Square number={board[5]} onClick={() => handlesquareClick(5)}/>
         </div>
         <div className="row">
-          <Square number={7} onClick={handlesquareClick}/>
-          <Square number={8} onClick={handlesquareClick}/>
-          <Square number={9} onClick={handlesquareClick}/>
+          <Square number={board[6]} onClick={() => handlesquareClick(6)}/>
+          <Square number={board[7]} onClick={() => handlesquareClick(7)}/>
+          <Square number={board[8]} onClick={() => handlesquareClick(8)}/>
         </div>
       </div>
       <div className="info">
         <div className="status">Next player: {isXNext ? "X" : "O"}</div>
         <Button className="">New Game</Button>
-
         <div className="history">
           <p>history</p>
           <Button>step 1</Button>
